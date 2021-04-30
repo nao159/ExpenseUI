6e156292dc59f91a75837c0d146f9fb53be82f1e
Maxim Vainikka <rithmique@yahoo.com>
2021-04-30 18:25:44 +0200

    Initial Commit

---
+++ ExpenseUI/ExpenseUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseUIApp.swift
+//  ExpenseUI
+//
+//  Created by Максим Нуждин on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
